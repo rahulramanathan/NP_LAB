bb92cfe4ec7c59509bbaf15c9a76b0295014d2fa
Rahul Ramanathan <26170036+rahulramanathan@users.noreply.github.com>
2018-02-19 11:42:32 +0530

    Add files via upload

--- LAB 6/clientl6q1.c
+++ LAB 6/clientl6q1.c
@@ -10,7 +10,7 @@
 #include<sys/stat.h>
 void main()
 {
-int s,r,recb,sntb,x;
+int s,r,recb,sntb,x,amt;
 printf("INPUT port number: ");
 scanf("%d", &x);
 struct sockaddr_in server;
@@ -32,5 +32,42 @@ printf("\nConnection error.");
 exit(0);
 }
 printf("\nSocket connected.");
-printf("\n\n");
-printf("")
+//program start
+printf("\nEnter username : ");
+scanf("%s",buff);
+sntb=send(s,buff,sizeof(buff),0);
+printf("\nEnter password : ");
+scanf("%s",buff);
+sntb=send(s,buff,sizeof(buff),0);
+recb=recv(s,buff,sizeof(buff),0);
+if(strcmp(buff,"0")==0)//user not found
+{
+	printf("\nUser not found\n");
+	close(s);
+	exit(0);
+}
+else
+ { //display menu
+	while(1)
+	{
+		printf("\nEnter 1 for debit\n2 for credit\n3 to view balance\n4 to exit\n");
+		scanf("%s",buff);
+		sntb=send(s,buff,sizeof(buff),0);//send choice to server
+		if(strcmp(buff,"4")==0)//exit
+		{
+			printf("Goodbye");
+			close(s);
+			exit(0);
+		}
+		else if(strcmp(buff,"1")==0 || strcmp(buff,"2")==0)//ask for balance
+		{
+			printf("Enter amount\n");
+			scanf("%d",amt);			
+		}
+		else//view balance
+		{
+
+		}
+	}	
+ }
+}
--- LAB 6/serverl6q1.c
+++ LAB 6/serverl6q1.c
@@ -8,6 +8,95 @@
 struct Bank
 {	
 	char username[50];
-	char password[50];
+	char password[50];//stored in encrypted format
 	float balance;
 };
+char * decode(char *str)
+{
+
+}
+void main()
+{
+	struct Bank b1[3];
+	strcpy(b1[0].username,"Samar");
+	strcpy(b1[1].username,"Sarthake");
+	strcpy(b1[2].username,"Rahul");
+	strcpy(b1[0].password,"pwwq");//mttn
+	strcpy(b1[1].password,"pdqdv");//manas
+	strcpy(b1[2].password,"srvw");//post
+	b1[0].balance=1000;
+	b1[1].balance=5000;
+	b1[2].balance=19000;
+	int s,r,recb,sntb,x,ns,a=0,i;
+	printf("INPUT port number: ");
+	scanf("%d", &x);
+	socklen_t len;
+	struct sockaddr_in server,client;
+	char buff[50],usr[50],pass[50];
+	s=socket(AF_INET,SOCK_STREAM,0);
+	if(s==-1)
+	{
+
+		printf("\nSocket creation error.");
+		exit(0);
+	}
+
+	printf("\nSocket created.");
+	server.sin_family=AF_INET;
+	server.sin_port=htons(x);
+	server.sin_addr.s_addr=htonl(INADDR_ANY);
+
+	r=bind(s,(struct sockaddr*)&server,sizeof(server));
+
+	if(r==-1)
+	{
+		printf("\nBinding error.");
+		exit(0);
+	}
+
+	printf("\nSocket binded.");
+	r=listen(s,1);
+
+	if(r==-1)
+	{
+		close(s);
+		exit(0);
+	}
+
+	printf("\nSocket listening.");
+	len=sizeof(client);
+	ns=accept(s,(struct sockaddr*)&client, &len);//client
+	if(ns==-1)
+	{
+		close(s);
+		exit(0);
+	}
+
+	printf("\nSocket accepting\nReading username");
+	recb=recv(ns,buff,sizeof(buff),0);//receive from client
+	strcpy(usr,buff);
+	printf("\nReading password");	
+	recb=recv(ns,buff,sizeof(buff),0);
+	strcpy(pass,buff);
+	for(i=0;i<3;i++)
+	{
+		if((strcmp(b1[i].username,usr)==0)&&(strcmp(b1[i].password,pass)==0))//found
+		{
+			x=1;
+			break;			
+		}
+	}
+	if(x==1)
+	{
+		printf("Valid\nBalance = %f",b1[i].balance);
+		strcpy(buff,"1");
+	}
+	else
+	{
+		printf("Invalid\n");
+		strcpy(buff,"0");
+	}
+	sntb=send(ns,buff,sizeof(buff),0);
+	close(ns);
+	close(s);
+}
